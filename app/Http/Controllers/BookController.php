e5445a277fd7415f16f0d18a779a9a6fdba5a29c
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-16 10:51:59 +0300

    invalidating cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,8 +28,6 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-//        $books = $books->get();
-
         $cacheKey = 'books:' . $filter . ':' . $title;
         $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
@@ -57,13 +55,15 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show',
-            ['book' => $book->load([
-                'reviews' => fn($query) => $query->latest()
-                ])
-            ]
+        $cacheKey = 'book:' . $book->id;
+
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn() => $book->load(['reviews' => fn($query) => $query->latest()])
         );
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
