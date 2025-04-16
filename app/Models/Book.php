a0eeb33c1c865423d4bc8e51e363fd8e40938c31
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-16 11:48:00 +0300

    rating and review count of all pages

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -37,24 +37,12 @@ class Book extends Model
 
     public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withCount(
-            [
-                'reviews' => fn(Builder $q)
-                => $this->dateRangeFilter($q, $from, $to)
-            ]
-        )
-            ->orderBy('reviews_count', 'desc');
+        return $query->withReviewsCount()->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withAvg(
-            [
-                'reviews' => fn(Builder $q)
-                => $this->dateRangeFilter($q, $from, $to)
-            ],
-            'rating'
-        )
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -96,5 +84,15 @@ class Book extends Model
         return $query->highestRated(now()->subMonths(6), now())
             ->popular(now()->subMonths(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted()
+    {
+        static::updated(
+            fn(Book $book) => cache()->forget('book:' .$book->id)
+        );
+        static::deleted(
+            fn(Book $book) => cache()->forget('book:' .$book->id)
+        );
     }
 }
